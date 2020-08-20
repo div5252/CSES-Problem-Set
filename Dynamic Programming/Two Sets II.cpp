5b63a45c58a46fde0e41b0b8737c2e041a1e11b1
div5252 <54735797+div5252@users.noreply.github.com>
2020-08-20 14:53:06 +0530

    Create Two Sets II.cpp

---
+++ Dynamic Programming/Two Sets II.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define vll vector<ll>
+#define ld long double
+#define pll pair<ll,ll>
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define osetll tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define ook order_of_key
+#define fbo find_by_order
+const int MOD=1000000007;
+long long int inverse(long long int i){
+    if(i==1) return 1;
+    return (MOD - ((MOD/i)*inverse(MOD%i))%MOD+MOD)%MOD;
+}
+ll POW(ll a,ll b)
+{
+    if(b==0) return 1;
+    if(b==1) return a%MOD;
+    ll temp=POW(a,b/2);
+    if(b%2==0) return (temp*temp)%MOD;
+    else return (((temp*temp)%MOD)*a)%MOD;
+}
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll n;
+    cin>>n;
+    ll tot=(n*(n+1))/2;
+    if(tot%2==1)
+    {
+        cout<<"0";
+        return 0;
+    }
+    tot/=2;
+    vector<vector<int> > dp(n+1,vector<int> (tot+1,0));
+    dp[0][0]=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<=tot;j++)
+        {
+            dp[i][j]=dp[i-1][j];
+            if(j-i>=0)
+            {
+                dp[i][j]=(dp[i][j]+dp[i-1][j-i])%MOD;
+            }
+        }
+    }
+    cout<<(dp[n][tot]*inverse(2))%MOD;
+}
