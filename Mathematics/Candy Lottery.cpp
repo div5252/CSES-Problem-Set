dcb4daf0490a5ff276384da0587e05d7cc9bc604
div5252 <54735797+div5252@users.noreply.github.com>
2020-08-31 12:28:01 +0530

    Create Candy Lottery.cpp

---
+++ Mathematics/Candy Lottery.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define vll vector<ll>
+#define ld long double
+#define pll pair<ll,ll>
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define osetll tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define ook order_of_key
+#define fbo find_by_order
+const int MOD=1000000007;
+long long int inverse(long long int i){
+    if(i==1) return 1;
+    return (MOD - ((MOD/i)*inverse(MOD%i))%MOD+MOD)%MOD;
+}
+ll POW(ll a,ll b)
+{
+    if(b==0) return 1;
+    if(b==1) return a%MOD;
+    ll temp=POW(a,b/2);
+    if(b%2==0) return (temp*temp)%MOD;
+    else return (((temp*temp)%MOD)*a)%MOD;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll n,k;
+    cin>>n>>k;
+    ld ans=0;
+    for(int i=1;i<=k;i++)
+    {
+        ld add=1,sub=1;
+        for(int j=1;j<=n;j++)
+        {
+            add*=(ld)i/(ld)k;
+            sub*=(ld)(i-1)/(ld)k;
+        }
+        ans+=(ld)(i)*(ld)(add-sub);
+    }
+    ans*=1e6;
+    ans=llround(ans);
+    ans/=1e6;
+    cout<<fixed<<setprecision(6)<<ans;
+}
