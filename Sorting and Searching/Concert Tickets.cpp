1ff006d1951ab8f9398696d8c538c21cddb67435
div5252 <54735797+div5252@users.noreply.github.com>
2021-04-30 14:09:15 +0530

    Create Concert Tickets.cpp

---
+++ Sorting and Searching/Concert Tickets.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define vll vector<ll>
+#define ld long double
+#define pll pair<ll,ll>
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define osetll tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define ook order_of_key
+#define fbo find_by_order
+const int MOD=1000000007; //998244353
+long long int inverse(long long int i){
+    if(i==1) return 1;
+    return (MOD - ((MOD/i)*inverse(MOD%i))%MOD+MOD)%MOD;
+}
+ll POW(ll a,ll b)
+{
+    if(b==0) return 1;
+    if(b==1) return a%MOD;
+    ll temp=POW(a,b/2);
+    if(b%2==0) return (temp*temp)%MOD;
+    else return (((temp*temp)%MOD)*a)%MOD;
+}
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll n,m;
+    cin>>n>>m;
+    ll h[n],p[m];
+    multiset<ll> s;
+    for(int i=0;i<n;i++)
+    {
+        cin>>h[i];
+        s.insert(h[i]);
+    }
+    for(int i=0;i<m;i++)
+    {
+        cin>>p[i];
+        auto it=s.upper_bound(p[i]);
+        if(it==s.begin()) cout<<"-1";
+        else 
+        {
+            it--;
+            cout<<(*it);
+            s.erase(it);
+        }
+        cout<<"\n";
+    }
+}
