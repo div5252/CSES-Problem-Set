64fbcd35b3aa6df5534f6df3053ec82191812cda
div5252 <54735797+div5252@users.noreply.github.com>
2021-05-04 14:07:08 +0530

    Create Number Spiral.cpp

---
+++ Introductory Problems/Number Spiral.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define vll vector<ll>
+#define ld long double
+#define pll pair<ll,ll>
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define osetll tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define ook order_of_key
+#define fbo find_by_order
+const int MOD=1000000007; //998244353
+long long int inverse(long long int i){
+    if(i==1) return 1;
+    return (MOD - ((MOD/i)*inverse(MOD%i))%MOD+MOD)%MOD;
+}
+ll POW(ll a,ll b)
+{
+    if(b==0) return 1;
+    if(b==1) return a%MOD;
+    ll temp=POW(a,b/2);
+    if(b%2==0) return (temp*temp)%MOD;
+    else return (((temp*temp)%MOD)*a)%MOD;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        ll y,x;
+        cin>>y>>x;
+        if(x<y)
+        {
+            if(y%2==0) cout<<y*y-x+1;
+            else cout<<(y-1)*(y-1)+x;
+        }
+        else
+        {
+            if(x%2==1) cout<<x*x-y+1;
+            else cout<<(x-1)*(x-1)+y;
+        }
+        cout<<"\n";
+    }
+}
