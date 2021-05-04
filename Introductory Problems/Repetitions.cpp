3a139b0e8720b58cfb3de9e2fcd092e7dce29bb4
div5252 <54735797+div5252@users.noreply.github.com>
2021-05-04 13:17:34 +0530

    Create Repetitions.cpp

---
+++ Introductory Problems/Repetitions.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define vll vector<ll>
+#define ld long double
+#define pll pair<ll,ll>
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define osetll tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define ook order_of_key
+#define fbo find_by_order
+const int MOD=1000000007; //998244353
+long long int inverse(long long int i){
+    if(i==1) return 1;
+    return (MOD - ((MOD/i)*inverse(MOD%i))%MOD+MOD)%MOD;
+}
+ll POW(ll a,ll b)
+{
+    if(b==0) return 1;
+    if(b==1) return a%MOD;
+    ll temp=POW(a,b/2);
+    if(b%2==0) return (temp*temp)%MOD;
+    else return (((temp*temp)%MOD)*a)%MOD;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string s;
+    cin>>s;
+    char ch=s[0];
+    ll ans=1,c=1;
+    for(int i=1;i<s.size();i++)
+    {
+        if(s[i]!=ch)
+        {
+            ans=max(ans,c);
+            ch=s[i];
+            c=1;
+        }
+        else
+        {
+            c++;
+        }
+    }
+    ans=max(ans,c);
+    cout<<ans;
+}
