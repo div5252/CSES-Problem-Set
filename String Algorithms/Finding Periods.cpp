8025dc287d9a0693c6f9b93066e6a52a23ca973d
div5252 <54735797+div5252@users.noreply.github.com>
2021-05-06 22:50:51 +0530

    Create Finding Periods.cpp

---
+++ String Algorithms/Finding Periods.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define vll vector<ll>
+#define ld long double
+#define pll pair<ll,ll>
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define oset tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define osetll tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define ook order_of_key
+#define fbo find_by_order
+const int MOD=1000000007; //998244353
+long long int inverse(long long int i){
+    if(i==1) return 1;
+    return (MOD - ((MOD/i)*inverse(MOD%i))%MOD+MOD)%MOD;
+}
+ll POW(ll a,ll b)
+{
+    if(b==0) return 1;
+    if(b==1) return a%MOD;
+    ll temp=POW(a,b/2);
+    if(b%2==0) return (temp*temp)%MOD;
+    else return (((temp*temp)%MOD)*a)%MOD;
+}
+
+vector<int> z_function(string s) {
+    int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+    return z;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string s;
+    cin>>s;
+    ll n=s.size();
+    vector<int> z=z_function(s);
+    for(int i=0;i<n;i++)
+    {
+        if(i+z[i]==n) cout<<i<<" ";
+    }
+    cout<<n;
+}
